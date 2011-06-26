a32d3e6f5c2f8fbbd5a587df009c3d092882639f
SLaks <Schabse@GMail.com>
2011-06-26 15:35:48 -0400

    IProgressReporter & empty implementation

---
+++ SLaks.ProgressReporting/EmptyProgressReporter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SLaks.ProgressReporting {
+	///<summary>A placeholder ProgressReporter implementation that doesn't do anything.</summary>
+	///<remarks>
+	///	<para>
+	///	 This class exists to allow progress-reporting methods to be called without progress.
+	///	</para>
+	///	<para>
+	///	 Methods should take an <see cref="IProgressReporter"/> as an optional parameter and default to <see cref="EmptyProgressReporter"/>.
+	///	 <code>
+	///		public static void Example(IProgressReporter progress) {	
+	///	 	progress = progress ?? new EmptyProgressReporter();
+	///	 	progress.Caption = "Demonstrating an example";
+	///	 }</code>
+	///	</para>
+	///</remarks>
+	public class EmptyProgressReporter : IProgressReporter {
+		///<summary>Gets or sets a string describing the current operation.  This property has no effect.</summary>
+		public string Caption { get; set; }
+
+		///<summary>Gets or sets the progress value at which the operation will be completed, or -1 to display a marquee.  This property has no effect.</summary>
+		public long Maximum { get; set; }
+		///<summary>Gets or sets the current progress, between 0 and Maximum.  This property has no effect.</summary>
+		public long Progress { get; set; }
+
+		///<summary>Always returns false.</summary>
+		public bool AllowCancellation {
+			get { return false; }
+			set { }
+		}
+
+		///<summary>Always returns false.</summary>
+		public bool WasCanceled { get { return false; } }
+	}
+}
---
+++ SLaks.ProgressReporting/IProgressReporter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SLaks.ProgressReporting {
+	///<summary>Displays the progress of an operation.</summary>
+	///<remarks>
+	///	<para>
+	///	A method that performs a lengthy operation can take an IProgressReporter 
+	///	to allow callers to display the operation's progress in the UI, without coupling 
+	///	the method to any UI framework.
+	///	</para>
+	///	<para>
+	///	 Methods should take an <see cref="IProgressReporter"/> as an optional parameter and default to <see cref="EmptyProgressReporter"/>.
+	///	 <code>
+	///		public static void Example(IProgressReporter progress) {	
+	///	 	progress = progress ?? new EmptyProgressReporter();
+	///	 	progress.Caption = "Demonstrating an example";
+	///	 }</code>
+	///	</para>
+	///</remarks>
+	public interface IProgressReporter {
+		///<summary>Gets or sets a string describing the current operation to display above the progress bar.</summary>
+		string Caption { get; set; }
+
+		///<summary>Gets or sets the progress value at which the operation will be completed, or -1 to display a marquee.</summary>
+		long Maximum { get; set; }
+		///<summary>Gets or sets the current progress, between 0 and Maximum.</summary>
+		long Progress { get; set; }
+
+		///<summary>Gets or sets whether the operation can be cancelled.  If this progress reporter does not allow users to cancel operations, this property will always return false.</summary>
+		bool AllowCancellation { get; set; }
+		///<summary>Indicates whether the user has cancelled the operation.</summary>
+		bool WasCanceled { get; }
+	}
+}
