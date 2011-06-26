a2d29843462749a93904a0e715364b5950c0a977
SLaks <Schabse@GMail.com>
2011-06-26 19:25:31 -0400

    Console progress bar

---
+++ SLaks.ProgressReporting/Display/ConsoleProgressReporter.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SLaks.ProgressReporting.Display {
+	///<summary>Prints a progress bar on the console.</summary>
+	///<remarks>
+	///The progress bar is one line tall, or two if configured to show a caption.
+	///</remarks>
+	public class ConsoleProgressReporter : ScaledProgressReporter, IProgressReporter {
+		#region Settings
+		readonly int originX, originY;
+
+		///<summary>Creates a ConsoleProgressReporter instance.</summary>
+		///<param name="showCaption">Whether to show the caption in the above the progress bar in the console.</param>
+		///<param name="width">The width of the bar in characters.  Defaults to fill the console window.</param>
+		[SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+		public ConsoleProgressReporter(bool showCaption = false, int width = -1) {
+			if (width > Console.BufferWidth - Console.CursorLeft)
+				throw new ArgumentOutOfRangeException("width", "Width must fit within the console window.");
+
+			ShowCaption = showCaption;
+			if (width < 0)
+				BarWidth = Console.WindowWidth - Console.CursorLeft;
+			else
+				BarWidth = width;
+
+			originX = Console.CursorLeft;
+			originY = Console.CursorTop;
+
+			UpdateBar(-1, ScaledValue);
+		}
+
+		///<summary>Indicates whether this instance has been configured to show a caption in the console.</summary>
+		public bool ShowCaption { get; private set; }
+		///<summary>Gets the width of the entire progress bar in characters.</summary>
+		public int BarWidth { get; private set; }
+		#endregion
+
+
+		static IDisposable CursorPosition(int x, int y) {
+			var retVal = CursorPosition();
+			Console.SetCursorPosition(x, y);
+			return retVal;
+		}
+		static IDisposable CursorPosition() {
+			int x = Console.CursorLeft, y = Console.CursorTop;
+			bool v = Console.CursorVisible;
+			Console.CursorVisible = false;
+			return new Disposable(() => {
+				Console.SetCursorPosition(x, y);
+				Console.CursorVisible = v;
+			});
+		}
+
+		#region Caption
+		string caption;
+
+		///<summary>Gets or sets a string describing the current operation to display above the progress bar.  The caption is only drawn if <see cref="ShowCaption"/> is true.</summary>
+		public string Caption {
+			get { return caption; }
+			set {
+				caption = value;
+				if (ShowCaption)
+					DrawCaption(TrimCaption(value));
+			}
+		}
+
+		string TrimCaption(string value) {
+			string retVal;
+			if (String.IsNullOrWhiteSpace(value))
+				retVal = "Please wait";
+			else
+				retVal = value.Trim();
+
+			if (retVal.Length > BarWidth)
+				retVal = retVal.Remove(BarWidth - 1);
+			return retVal;
+		}
+		void DrawCaption(string text) {
+			using (CursorPosition(originX + (BarWidth - text.Length) / 2, originY)) {
+				Console.Write(text);
+			}
+		}
+		#endregion
+
+		#region Bar
+		const char barChar = '█';
+		const char blankChar = '_';
+
+		///<summary>Gets the maximum that the progress bar's value will be scaled to.</summary>
+		protected override int ScaledMax { get { return BarWidth; } }
+
+		///<summary>Draws the progress bar to the console.</summary>
+		protected override void UpdateBar(int oldValue, int newValue) {
+			int barY = originY;
+			if (ShowCaption) barY++;
+
+			if (newValue < 0) {
+				//Marquee bar; there isn't much scope for a marquee bar on the console
+				//TODO: Optional animation; bounce a spinner back and forth.
+				using (CursorPosition(originX, barY))
+					DrawChars('▒', BarWidth);
+				return;
+			}
+
+			if (oldValue < 0) {
+				//Draw entire bar
+				using (CursorPosition(originX, barY)) {
+					DrawChars(barChar, newValue);
+					DrawChars(blankChar, BarWidth - newValue);
+				}
+			} else {
+				//Only draw the portion that changed
+				if (newValue > oldValue) {
+					using (CursorPosition(originX + oldValue, barY))
+						DrawChars(barChar, newValue - oldValue);
+				} else {
+					using (CursorPosition(originX + newValue, barY))
+						DrawChars(blankChar, oldValue - newValue);
+				}
+			}
+		}
+
+		static void DrawChars(char ch, int count) {
+			if (count == 0) return;
+			Console.Write(new string(ch, count));
+		}
+		#endregion
+
+		///<summary>Gets or sets whether the operation can be cancelled.</summary>
+		public bool AllowCancellation { get; set; }
+
+		bool wasAlreadyCanceled;
+		///<summary>Indicates whether the user has cancelled the operation.</summary>
+		public bool WasCanceled {
+			get { return AllowCancellation && wasAlreadyCanceled || (wasAlreadyCanceled = Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.C); }
+		}
+	}
+}
---
+++ SLaks.ProgressReporting/Display/ScaledProgressReporter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SLaks.ProgressReporting.Display {
+	///<summary>A base class for a progress reporter that scales progress values to a fixed maximum.</summary>
+	public abstract class ScaledProgressReporter {
+		long progress = 0, maximum = 100;
+
+		///<summary>Gets the maximum that the progress bar's value will be scaled to.</summary>
+		protected abstract int ScaledMax { get; }
+		///<summary>Gets the value of the progress bar, scaled to ScaledMax.</summary>
+		protected int ScaledValue { get; private set; }
+
+		private void Update() {
+			int oldValue = ScaledValue;
+			int newValue = Maximum < 0 ? -1 : (int)((double)Progress / Maximum * ScaledMax);
+			if (newValue == oldValue) return;
+
+			ScaledValue = newValue;
+			UpdateBar(oldValue, newValue);
+		}
+		///<summary>Draws an updated progress bar.</summary>
+		protected abstract void UpdateBar(int oldValue, int newValue);
+
+		///<summary>Gets or sets the progress value at which the operation will be completed, or -1 to display a marquee.</summary>
+		public long Maximum {
+			get { return maximum; }
+			set {
+				if (value < 0)
+					value = -1;
+				else if (value == 0)
+					throw new ArgumentOutOfRangeException("value");
+
+				maximum = value;
+				value = 0;
+				Update();
+			}
+		}
+
+		///<summary>Gets or sets the current progress, between 0 and Maximum.</summary>
+		public long Progress {
+			get { return progress; }
+			set {
+				if (value < 0 || value > Maximum)
+					throw new ArgumentOutOfRangeException("value", "Progress must be between 0 and " + Maximum);
+				progress = value;
+				Update();
+			}
+		}
+	}
+}
