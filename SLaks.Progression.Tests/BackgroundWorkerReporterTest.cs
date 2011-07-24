460def2b6820c92ca26cdc4a99671cb43195f1de
SLaks <Schabse@GMail.com>
2011-07-24 16:58:44 -0400

    BackgroundWorkerReporter

---
+++ SLaks.Progression.Tests/BackgroundWorkerReporterTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace SLaks.Progression.Tests {
+	[TestClass]
+	public class BackgroundWorkerReporterTest : ProgressReporterTestBase {
+		protected override IProgressReporter CreateReporter() {
+			return new Display.BackgroundWorkerReporter(new BackgroundWorker());
+		}
+
+		public override bool SupportsCancellation { get { return true; } }
+
+		[TestMethod]
+		public void ProgressWorks() {
+			var bwr = new Display.BackgroundWorkerReporter(new BackgroundWorker());
+
+			int workerProgress = -1;
+			bwr.Worker.ProgressChanged += (s, e) => { workerProgress = e.ProgressPercentage; };
+
+			bwr.Maximum = 50;
+			bwr.Progress = 10;
+
+			Assert.AreEqual(20, workerProgress);
+		}
+
+		[TestMethod]
+		public void CancellationWorks() {
+			var bwr = new Display.BackgroundWorkerReporter(new BackgroundWorker());
+			bwr.AllowCancellation = true;
+			bwr.Worker.CancelAsync();
+			Assert.IsTrue(bwr.WasCanceled);
+		}
+	}
+}
---
+++ SLaks.Progression/Display/BackgroundWorkerReporter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace SLaks.Progression.Display {
+	///<summary>Reports progress through an existing BackgroundWorker.  Does not support captions.</summary>
+	///<remarks>
+	/// You are responsible for handling the BackgroundWorker's ProgressChagned events and updating UI
+	///<para>
+	///	This class is primarily intended to be used with existing code that
+	///	already uses a BackgroundWorker.  New code can usually use other 
+	///	progress reporters to update the UI directly.
+	///</para>
+	///</remarks>
+	public class BackgroundWorkerReporter : ScaledProgressReporter, IProgressReporter {
+		///<summary>Creates a BackgroundWorkerReporter instance that reports progress to the specified BackgroundWorker.</summary>
+		public BackgroundWorkerReporter(BackgroundWorker worker) {
+			if (worker == null) throw new ArgumentNullException("worker");
+			Worker = worker;
+			worker.WorkerReportsProgress = true;
+		}
+
+		///<summary>Gets the BackgroundWorker instance that this reporter updates.</summary>
+		public BackgroundWorker Worker { get; private set; }
+
+		///<summary>Gets the maximum that the progress bar's value will be scaled to.</summary>
+		protected override int ScaledMax { get { return 100; } }
+
+		///<summary>Updates the BackgroundWorker's progress.</summary>
+		protected override void UpdateBar(int? oldValue, int? newValue) {
+			if (newValue != null)
+				Worker.ReportProgress(newValue.Value);
+		}
+
+		///<summary>Gets or sets a caption for the current operation.  This property is ignored.</summary>
+		public string Caption { get; set; }
+
+		///<summary>Gets or sets whether the operation can be cancelled.  The default is false.</summary>
+		///<remarks>Setting this property will reset <see cref="WasCanceled"/>.</remarks>
+		public bool AllowCancellation {
+			get { return Worker.WorkerSupportsCancellation; }
+			set { Worker.WorkerSupportsCancellation = value; }
+		}
+
+		///<summary>Indicates whether the user has cancelled the operation.</summary>
+		public bool WasCanceled { get { return Worker.CancellationPending; } }
+	}
+}
